6ed4db5cc38a12e9c22df9f998330a6eca21d247
Hajaraddi <addihajar5@gmail.com>
2025-06-16 18:38:06 +0100

    Add the space

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -8,6 +8,6 @@ def safe_divide(numerator, denominator):
             return "Error: Cannot divide by zero."
         
         result = num / den
-        return f"The result of the division is{result}"
+        return f"The result of the division is {result}"
     except ValueError:
         return "Error: Please enter numeric values only."
