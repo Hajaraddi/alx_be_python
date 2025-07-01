d4e443b6365a0beba3488360eb0f8ae787b2b8ac
Hajaraddi <addihajar5@gmail.com>
2025-07-02 00:00:56 +0100

    Add the file dail_reminder.py to the folder control-flow

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,26 @@
+# daily_reminder.py
+
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time_bound? (yes/no): ").lower()
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+        else:
+            print(f"Reminder: '{task}' is a high priority task. Tru to complete it as soon as possible.")   
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a medium priority task that requires attention today.")
+        else:
+            print(f"Reminder: '{task}' is a medium priority task. Schedule if when convenient.")    
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a low priority task still needs to be done today.")    
+        else:
+            print(f"Note: '{task}' is a low priority task. Consider completing it xhen you have free time.")    
+    case _:
+        print("Invalid priority level. Please enter high, medium, or low.")
+
+        
