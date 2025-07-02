2efbaabe5fa7c249c7734589727a13e11657c2cd
Hajaraddi <addihajar5@gmail.com>
2025-07-02 21:01:27 +0100

    Correct the C in Celcius by S

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,17 +1,17 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELCIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return celsius * CELCIUS_TO_FAHRENHEIT_FACTOR +32
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR +32
 
 def main():
     try:
         temp_input = input("Enter the temperature to convert: ")
         temperature = float(temp_input)
-        unit = input("Is this temperature in Celcius or Fahrenheit? (C/F): ")
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
         if unit == "F":
             converted = convert_to_celsius(temperature)
             print(f"{temperature}°F is {converted}°C")
